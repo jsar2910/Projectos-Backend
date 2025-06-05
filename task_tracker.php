4347761358358b305f848ccba933b7cb5307350d
jsar2910 <jsar2910@gmail.com>
2025-06-05 15:48:51 +0200

    definido obtener todos los task

--- task_tracker.php
+++ task_tracker.php
@@ -1,3 +1,24 @@
 <?php
-    echo "Hola";
-?>
+   $file_path = __DIR__."/task.json";
+
+   if (!file_exists("task.json")) {
+        file_put_contents($archivo,json_encode([]));
+   }
+ 
+   $file_task = fopen($file_path, 'rw');
+   $json = file_get_contents($file_path);
+   $data = json_decode($json,true);
+   
+   
+   switch ($argv[1]) {
+      case 'list':
+         foreach ($data as $task) {
+            echo "id task: ".$task['id']."\n";
+            echo "name task: ".$task['name']."\n";
+            echo "status task: ".$task['status']."\n";
+         }
+         break;
+      case 'add':
+         
+         break;
+   }
